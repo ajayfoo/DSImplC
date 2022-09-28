736fd0b63f47f349da5b1bf79c203569b5a5f2a8
Ajay K <76083469+ajaypep@users.noreply.github.com>
2022-09-28 19:11:23 +0530

    Add empty files for Dynamic Array.
    Signed-off-by: Ajay K <76083469+ajaypep@users.noreply.github.com>

---
+++ Arrays/Dynamic/src/dynamic_array/dynamic_array.c
@@ -0,0 +1,9 @@
+//
+// Created by ajay on 9/28/22.
+//
+#include "dynamic_array.h"
+
+int get_one()
+{
+    return 1;
+}
---
+++ Arrays/Dynamic/src/exec_program.c
@@ -0,0 +1,10 @@
+
+#include "dynamic_array.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    printf("One: %d\n",get_one());
+    return 0;
+}

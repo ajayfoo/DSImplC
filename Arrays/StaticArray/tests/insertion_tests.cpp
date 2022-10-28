a93d623359dc68edb57a5d4b7c8e0a885d6e09c0
Ajay K <76083469+ajaypep@users.noreply.github.com>
2022-10-28 20:45:04 +0530

    Fix empty array test. Add build* dir to gitignore. Add cmake presets.

--- Arrays/StaticArray/tests/insertion_tests.cpp
+++ Arrays/StaticArray/tests/insertion_tests.cpp
@@ -3,13 +3,12 @@
 //
 #include "gtest/gtest.h"
 
-extern "C"
-{
+extern "C" {
 #include "static_array.h"
 }
 
-TEST(InsertionTests,EmptyArray)
-{
-    StaticArray static_array;
-    ASSERT_EQ(static_array.m_length,0);
+TEST(InsertionTests, EmptyArray) {
+  StaticArray static_array;
+  init_static_array(&static_array);
+  ASSERT_EQ(static_array.m_length, 0);
 }
